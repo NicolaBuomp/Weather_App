81292a26dc4e8e548fd1ed1f31ea212fa0f644ec
Nicola Buompane <61230893+NicolaBuomp@users.noreply.github.com>
2025-03-15 19:10:10 +0100

    Initial Commit

---
+++ Weather_App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather_App
+//
+//  Created by Nicola Buompane on 15/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
